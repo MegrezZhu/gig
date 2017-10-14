409e8691ab30b6f4c5beb9347e17bfe80021a59b
Zhu Chu <mystery490815101@gmail.com>
2017-10-14 14:04:20 +0800

    1.0.0

--- index.js
+++ index.js
@@ -2,7 +2,22 @@
 const program = require('commander');
 
 program
-  .version(require('./package.json').version)
-  .option('-p, --proxy <host>', 'using proxy');
+  .version(require('./package.json').version);
+
+program
+  .command('l')
+  .option('-p, --proxy <host>', 'using proxy')
+  .description('list all .gitignore template names')
+  .action(require('./lib/list'));
+
+program
+  .command('d <template>')
+  .description('download specific .gitignore template')
+  .option('-p, --proxy <host>', 'using proxy')
+  .action(require('./lib/download'));
 
 program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
