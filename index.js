306426246e2bb7e01201d47610fe239664114900
Zhu Chu <mystery490815101@gmail.com>
2017-10-15 15:01:13 +0800

    v 1.1

--- index.js
+++ index.js
@@ -7,18 +7,22 @@ program
 
 program
   .command('u')
+  .alias('update')
   .option('-p, --proxy <host>', 'using proxy')
   .description('fetch & update cached templates')
   .action(require('./lib/update'));
 
 program
   .command('l')
-  .description('list all .gitignore template names')
+  .alias('list')
+  .description('list all available .gitignore template')
   .action(require('./lib/list').handler);
 
 program
   .command('g <template>')
+  .alias('gen')
   .option('-s, --stdout', 'print to stdout instead of file')
+  .option('-n, --noAuto', 'disable automatic template name completion')
   .description('generate .gitignore with specified template')
   .action(require('./lib/generate'));
 
